6aeb0cffe28a82b0658822fbec8c78ded3cb71fc
aibornstore <apohob5@gmail.com>
2026-08-06 14:26:56 +0300

    V28: RU/EN language toggle on all 437 pages + i18n.js

---
+++ _tmp_check.py
@@ -0,0 +1,5 @@
+import re, json, os
+from collections import defaultdict, Counter
+
+base = chr(68)+chr(58)+chr(47)+chr(52)+chr(47)+chr(67)+chr(76)+chr(69)+chr(65)+chr(78)+chr(95)+chr(65)+chr(82)+chr(67)+chr(72)+chr(73)+chr(86)+chr(69)
+
